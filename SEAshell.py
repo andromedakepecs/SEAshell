e1b5e4b261d24c4939b89714fb54ba87255b9877
andromeda <andromeda@andromedas-MacBook-Air.local>
2021-03-11 00:36:19 -0800

    some more working fn

--- SEAshell.py
+++ SEAshell.py
@@ -1,6 +1,5 @@
 import os
 import subprocess
-import re
 
 '''
 SEAshell
@@ -11,10 +10,11 @@ Last Modified: 3/10/2021
 
 # Colored text
 class col:
-	BAD = '\033[91m'	# Warnings, exceptions, etc.
-	IN = '\u001b[37m'	# User input
-	OUT = '\u001b[32m'	# Shell output
-	PROMPT = '\033[96m'	# Shell prompt
+	BAD = '\033[91m'	# Warnings, exceptions, etc. (red)
+	IN = '\u001b[37m'	# User input (white)
+	OUT = '\u001b[32m'	# Shell output (green)
+	PROMPT = '\033[96m'	# Shell prompt (cyan)
+	BOLD = '\u001b[35m'	# Bold (magenta)
 
 # Creates tokens from user input, accounting for quotes and backslashes
 def tokenize(commands):
@@ -51,23 +51,59 @@ def tokenize(commands):
 	temp1 = [item.replace('$', ' ') for item in temp]
 	tokens = [item.replace('%', '"') for item in temp1]
 
-# Parses and executes
-def execute_commands(commands):
-	command_tokens = tokenize(commands)
-	print(command_tokens)
+	return tokens
 
+# Parses and executes commands
+# TODO: wild cards, flags maybe
+def execute_commands(commands):
+	tokens = tokenize(commands)
+	if len(tokens) == 1:
+		inp = tokens[0]
+		if inp == "pwd":	# Working directory filepath
+			pwd()
+		elif inp == "jobs":	# Running processes
+			print(col.OUT + str(jobs()))
+		elif inp == "cd":	# Home directory
+			cd("home")
+		elif inp == "ls":
+			ls()
+	elif len(tokens) == 2:
+		if tokens[0] == "cd":
+			cd(tokens[1])
+	
+# TODO
 def expand(wildcard):
 	pass
 
+# Print current working directory
 def pwd():
-	return str(os.getcwd())
+	print(col.OUT + str(os.getcwd()))
 
+# Returns currently running processes
+def jobs():
+	output = subprocess.Popen(['ps', '-U', '0'], stdout=subprocess.PIPE).communicate()[0]
+	return output
+
+# Lists files in current directory
+def ls():
+	output = str(subprocess.call("ls"))
+	return output[:-1]	# Hides exit status
+
+# TODO
+def cd(path):
+	if path == "home":
+		home = os.path.expanduser('~')
+		os.chdir(home)
+		
+
+
+# TODO
 def help():
-	print(col.OUT + "hi")
+	print(col.OUT + "help")
 
 # Main
-print(col.OUT + "Welcome to the Swaggy Elementary Andromeda shell")
 def main():
+	print(col.BOLD + "Welcome to the Swaggy Elementary Andromeda shell")
 	while True:
 		commands = input(col.PROMPT + "SEAshell:" + str(os.path.basename(os.getcwd())) + " " + os.getlogin() + "$ " + col.IN)
 		if commands == "exit":
